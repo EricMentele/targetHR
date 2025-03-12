d132022d1b4a719956c7115fc2f96e531840043f
Eric Mentele <ericmentele@gmail.com>
2025-03-11 21:07:21 -0500

    Initial Commit

---
+++ targethr Watch App/targethrApp.swift
@@ -0,0 +1,17 @@
+//
+//  targethrApp.swift
+//  targethr Watch App
+//
+//  Created by Eric Mentele on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct targethr_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ targethr Watch AppTests/targethr_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  targethr_Watch_AppTests.swift
+//  targethr Watch AppTests
+//
+//  Created by Eric Mentele on 3/11/25.
+//
+
+import Testing
+@testable import targethr_Watch_App
+
+struct targethr_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
